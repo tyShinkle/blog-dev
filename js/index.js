31fc49e757f35ec13997fe00df19e9b954341278
Ty Shinkle <65380571+tyShinkle@users.noreply.github.com>
2023-04-12 11:01:54 -0500

    cleaner blog

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import { buildPosts } from "./buildPosts.js";
+import { empowerPosts } from "./postLogic.js";
+import { empowerWindow } from "./windowLogic.js";
+
+buildPosts();
+empowerPosts();
+empowerWindow();
